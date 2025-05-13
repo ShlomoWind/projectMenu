2984b6fb152fe4c036ca5c007020ab33256e7800
Shlomo <sh.z.wind@gmail.com>
2025-05-13 11:30:00 +0300

    changes by the functions & ready to merging

--- projectMenu/Program.cs
+++ projectMenu/Program.cs
@@ -42,6 +42,48 @@ namespace projectMenu
             }
             return false;
         }
+        static void Menu()            //מציג את התפריט
+        {
+            Console.WriteLine("enter your choose: \na. to input a series \nb. to display the series \nc. to display the series in revers \nd. to display sorted seriec \ne. to display the max number \nf. to display the min number \ng. to display the average \nh. to display the number of elements \ni. to display the sum of the numbers \nj. to exit");
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    InputSeries();
+                    break;
+                case "b":
+                    ShowSeries();
+                    break;
+                case "c":
+                    ReversSeries();
+                    break;
+                case "d":
+                    SortedSeries();
+                    break;
+                case "e":
+                    HighestNumber();
+                    break;
+                case "f":
+                    LowestNumber();
+                    break;
+                case "g":
+                    AverageNumber();
+                    break;
+                case "h":
+                    ElementsNumber();
+                    break;
+                case "i":
+                    SumNumber();
+                    break;
+                case "j":
+                    Exit();
+                    break;
+                default:
+                    Menu();
+                    break;
+
+            }
+
+        }
         static void InputSeries()            //בקשה מהמשתמש לסדרה שתדרוס את הסדרה הקיימת והמחזיר את הסדרה שהוא הכניס
         {
             Console.WriteLine("a");
@@ -84,48 +126,6 @@ namespace projectMenu
         }
         static void Main(string[] args)
         {
-            void Menu()            //מציג את התפריט
-            {
-                Console.WriteLine("enter your choose: \na. to input a series \nb. to display the series \nc. to display the series in revers \nd. to display sorted seriec \ne. to display the max number \nf. to display the min number \ng. to display the average \nh. to display the number of elements \ni. to display the sum of the numbers \nj. to exit");
-                switch (Console.ReadLine())
-                {
-                    case "a":
-                        InputSeries();
-                        break;
-                    case "b":
-                        ShowSeries();
-                        break;
-                    case "c":
-                        ReversSeries();
-                        break;
-                    case "d":
-                        SortedSeries();
-                        break;
-                    case "e":
-                        HighestNumber();
-                        break;
-                    case "f":
-                        LowestNumber();
-                        break;
-                    case "g":
-                        AverageNumber();
-                        break;
-                    case "h":
-                        ElementsNumber();
-                        break;
-                    case "i":
-                        SumNumber();
-                        break;
-                    case "j":
-                        Exit();
-                        break;
-                    default:
-                        Menu();
-                        break;
-
-                }
-
-            }
         }
     }
 }
